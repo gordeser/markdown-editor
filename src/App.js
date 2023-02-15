f204113d8b87947134218eb62c972c60db71b98d
Sergei Gordeev <gordeser@cvut.cz>
2023-02-15 01:18:50 +0100

    Add handler on change textarea

--- src/App.js
+++ src/App.js
@@ -4,9 +4,13 @@ import {useState} from "react";
 function App() {
     const [markdown, setMarkdown] = useState('# hello world')
 
+    const handleOnChange = (e) => {
+        setMarkdown(e.target.value)
+    }
+
     return (
         <div className="App">
-            <textarea value={markdown}/>
+            <textarea onChange={handleOnChange} value={markdown}/>
             <div className="preview">{markdown}</div>
         </div>
     );
